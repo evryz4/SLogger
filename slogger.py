e3210d710dc26b0d951aa4fdb41f8ea36decbdd1
watermelon46 <141306873+watermelon46@users.noreply.github.com>
2024-01-29 00:01:34 +0300

    1.2 + change_branding() + custom()
    • 1.2 Release
    • Added change_branding()
    • Added custom()
    • Now command description are in command!
    
    p. s. I'm bad at English, if there are mistakes in the text, I'm sorry.

--- slogger.py
+++ slogger.py
@@ -1,21 +1,9 @@
-# SLogger (SimpleLogger) library for easy logging
-# Official SLogger repository - https://github.com/watermelon46/slogger
-# Commands:
-
-# log(text) - write anything in logs
-# info(text) - write info in logs
-# warn(text) - write warn in logs
-# error(text) - write error in logs
-# user(text) - write user input in logs
-# printing(boolean) - enable additional printing logs in terminal
-
 class BuildInfo():
-    version = '1.1.1'
+    version = '1.2'
     buildType = 'Release'
     branding = 'SLogger'
+    brandingInLogs = f'[{branding}]'
     githublink = 'https://github.com/watermelon46/slogger'
-
-
 
 from datetime import datetime
 from time import sleep
@@ -26,31 +14,47 @@ logfile.truncate(0)
 
 enable_printing = True
 
+
+
+def change_branding(newbranding = None):
+    """Change branding in logs from ''Slogger'' to your."""
+    BuildInfo.brandingInLogs = f'[{newbranding}]'
+
 def log(text):
+    """Write anything in logs"""
     now = datetime.now()
     time = now.strftime("%H:%M:%S")
-
-    logtext = f'[{time}] [{BuildInfo.branding}] {text}'
+    logtext = f'[{time}] {BuildInfo.brandingInLogs} {text}'
     logfile.write(logtext + '\n')
     if enable_printing == True:
         print(logtext, end='')
 
 def warn(text):
+    """Write warn in logs"""
     log(f'[WARN] {text}')
 
 def error(text):
+    """Write error in logs"""
     log(f'[ERROR] {text}')
 
 def info(text):
+    """Write info in logs"""
     log(f'[INFO] {text}')
 
 def user(text):
+    """Write user input in logs"""
     log(f'[USER] {text}')
 
+def custom(logtype, text):
+    """Write custom log in logs"""
+    log(f'[{logtype}] {text}')
+    
 def printing(mode = True):
+    """Enable additional logs printing in terminal"""
     global enable_printing
     enable_printing = bool(mode)
     info(f'enable_printing changed to {mode}')
 
 info(f'{BuildInfo.branding} {BuildInfo.version} {BuildInfo.buildType} started')
-
+if BuildInfo.buildType != 'Release':
+    warn(f'You are using unstable version of SLogger. Download stable from {BuildInfo.githublink}')
